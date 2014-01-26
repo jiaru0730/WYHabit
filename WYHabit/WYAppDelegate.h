a5f49620429d60e4ab2256974c3f37815825f6af
JiaRu <lony1107@gmail.com>
2014-01-26 11:12:06 +0800

    Initial Commit

---
+++ WYHabit/WYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WYAppDelegate.h
+//  WYHabit
+//
+//  Created by Jia Ru on 1/26/14.
+//  Copyright (c) 2014 JiaruPrimer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WYHabit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WYHabit
+//
+//  Created by Jia Ru on 1/26/14.
+//  Copyright (c) 2014 JiaruPrimer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WYAppDelegate class]));
+    }
+}
