5146382d12c5cac5506cb175c8700318e788665e
JiaRu <lony1107@gmail.com>
2014-03-27 19:51:08 +0800

    ADD: WYDate.

---
+++ WYHabit/SourceCode/Common/Model/WYDate.h
@@ -0,0 +1,17 @@
+//
+//  WYDate.h
+//  WYHabit
+//
+//  Created by Jia Ru on 3/27/14.
+//  Copyright (c) 2014 JiaruPrimer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WYDate : NSObject
+
+@property (assign, nonatomic) int year;
+@property (assign, nonatomic) int month;
+@property (assign, nonatomic) int day;
+
+@end
---
+++ WYHabit/SourceCode/Common/Model/WYDate.m
@@ -0,0 +1,13 @@
+//
+//  WYDate.m
+//  WYHabit
+//
+//  Created by Jia Ru on 3/27/14.
+//  Copyright (c) 2014 JiaruPrimer. All rights reserved.
+//
+
+#import "WYDate.h"
+
+@implementation WYDate
+
+@end
