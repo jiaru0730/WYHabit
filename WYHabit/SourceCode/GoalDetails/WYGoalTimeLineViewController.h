789fb1b4f2b21a5db74ac638d24f64a171b3ddc4
JiaRu <lony1107@gmail.com>
2014-03-29 17:12:30 +0800

    ADD: goal time line table view controller.

---
+++ WYHabit/SourceCode/GoalDetails/WYGoalTimeLineViewController.h
@@ -0,0 +1,15 @@
+//
+//  WYGoalTimeLineViewController.h
+//  WYHabit
+//
+//  Created by Jia Ru on 3/29/14.
+//  Copyright (c) 2014 JiaruPrimer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYGoalTimeLineViewController : UITableViewController
+
+- (id)initWithGoal:(NSString *)goalID;
+
+@end
